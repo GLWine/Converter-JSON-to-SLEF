363a4183f202116fe56ad7930f34b951be2a0c5a
Giampiero <54741923+GLWine@users.noreply.github.com>
2024-05-25 17:39:31 +0200

    Create main.py

---
+++ src/Converter-JSON-to-SLEF_GLWine/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import parser_JSON  # noqa:F401
+import parser_SLEF  # noqa:F401
+
+
+def main():
+    print("Ciao, mondo!")
+
+
+if __name__ == "__main__":
+    main()
